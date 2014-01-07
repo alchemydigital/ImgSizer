ccc5eb32d17ceba418e3aadddc0ddb08dcd6f6be
Phil Stephens <email@philstephens.info>
2014-01-07 13:55:01 +1000

    Updated read me with simple instructions, added test image

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 ImageSizer
 ==========
 
-A simple application for resizing and caching images on-demand
+A simple application for resizing and caching images on-demand.
+
+Configurable to use either GD2 or ImageMagick libraries.  Can also include preset image sizes in the config file.
+
+##Installation
+
+Application is built on top of Slim and Imagine, so need to install those dependencies first.
+
+Install Composer and run `composer update` on this directory.
+
+Ensure that img/ is writable.
+
+Visit img/640x/uploads/test.jpg in browser.
+
+##Usage
+
+visit img/[derivative]/[path_to_file] e.g. img/640x/uploads/test.jpg
+
+Named derivative - uses the config file.
+N - Resizes image to fit into an NxN square whilst maintaining aspect ratio.
+Nx - Resizes image to N pixels wide while maintaining aspect ratio.
+xN - Resizes image to N pixels tall while maintaining aspect ratio.
+N1xN2 - Creates a cropped thumbnail of the image at the given size.
--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 require 'vendor/autoload.php';
 
+// Load the config file
 $config = json_decode(file_get_contents('config.json'), true);
 
+// Increase the memory limit if necessary
 if( ! empty($config['memory_limit']))
 {
+	// @todo check to see that the current memory limit is lower before setting a new one
 	ini_set('memory_limit', $config['memory_limit']);
 }
 
@@ -14,24 +17,31 @@ $app->get(
 		'/' . $config['image_dir'] . ':parts+',
 		function($parts) use ($config, $app)
 		{
-			// Grab the filename
+			// Get the filename
 			$filename = array_pop($parts);
 
-			// Grab the derivative
+			// Get the derivative
 			$derivative = array_shift($parts);
 
 			// Get the path
 			$path = implode("/", $parts);
 
-			// assemble the destination path
-			$destination_path = $config['image_dir'] . $derivative . '/' . $path;
+			// Set the source path
+			$source_path = $config['image_dir'] . $path;
 
-			// Create the directory, if required
-			if (!file_exists($destination_path)) {
-			    mkdir($destination_path, 0777, true);
+			// Does this file exist?
+			if ( ! file_exists($source_path)) {
+				// @todo Is it an image? e.g. if PDF, maybe create a thumbnail?
+				die;
 			}
 
-			$source_path = $config['image_dir'] . $path;
+			// Assemble the destination path
+			$destination_path = $config['image_dir'] . $derivative . '/' . $path;
+
+			// create the destination directory structure if required
+			if ( ! file_exists($destination_path)) {
+			    mkdir($destination_path, 0777, true);
+			}
 
 			if( ! empty($config['sizes'][$derivative]))
 			{
@@ -48,18 +58,23 @@ $app->get(
 				}
 			}
 
+			// We don't have any idea how to transform the image - let's bail!
 			if(empty($specs))
 			{
+				// @todo How to handle the lack of specs - serve the original image?
 				die;
 			}
 
-			// Do the transformations	
+			// Do the transformation(s)	
 			try {
 
 				$class = "Imagine\\{$config['library']}\\Imagine";
 
 			    $imagine = new $class;
 
+
+			    // Sometimes images are uploaded in the wrong orientation, but the true orientation can sometimes be found in the image EXIF data
+			    // Check to see if we can find it (JPEG and TIFF only)
 			    $rotateVal = 0;
 
 			    if(in_array(finfo_file(finfo_open(FILEINFO_MIME_TYPE), $source_path . '/' . $filename), array('image/jpeg', 'image/tiff')))
@@ -81,6 +96,8 @@ $app->get(
 				
 				if( ! empty($specs['width']) && ! empty($specs['height']))
 				{
+					// For some reason you need to chain the entire transformation in one go
+					// when using the thumbnail() command - hence the repitition below
 					$image = $imagine->open($source_path . '/' . $filename)
 									->rotate($rotateVal)
 									->thumbnail( new Imagine\Image\Box($specs['width'], $specs['height']), ( ! empty($specs['type'])) ? $specs['type'] : 'outbound')
@@ -90,7 +107,7 @@ $app->get(
 				{
 					$image = $imagine->open($source_path . '/' . $filename)->rotate($rotateVal);
 
-					if(! empty($specs['width']))
+					if( ! empty($specs['width']))
 					{
 						$image->resize($image->getSize()->widen( $specs['width'] ));
 					} else
@@ -98,6 +115,7 @@ $app->get(
 						$image->resize($image->getSize()->heighten( $specs['height'] ));
 					}
 
+					// Save image for direct access next time
 					$image->save($destination_path . '/' . $filename);
 				}
 
@@ -105,15 +123,17 @@ $app->get(
 			    // handle the exception
 			}
 
-			// set the headers; first, getting the Mime type
+			// Set the headers... 
+			// Get the mime type
 			$finfo = finfo_open(FILEINFO_MIME_TYPE);
 			$mime_type = finfo_file($finfo, $destination_path . '/' . $filename);
 			$app->response->headers->set('Content-Type', $mime_type);
 
-			// Get the file extension, so we know how to output the file
+			// Get the file extension so we know how to output the file
 			$path_info = pathinfo($filename);
 			$ext = $path_info['extension'];
 
+			// Output the resized image
 			echo $image;
 		}
 	);
